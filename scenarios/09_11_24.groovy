179953d7868de0de8f5ce75cbfce5fefa369b4fe
Sviatoslav Zastavskyi <zastavskyi.s@ajax.systems>
2024-11-06 17:46:47 +0200

    add stages.groovy

---
+++ scenarios/09_11_24.groovy
@@ -0,0 +1,18 @@
+def getStages() {
+    return [
+        [
+            name: 'Dynamic stage 5',
+            steps: {}
+        ],
+        [
+            name: 'Dynamic stage 6',
+            steps: {}
+        ],
+        [
+            name: 'Dynamic stage 7',
+            steps: {}
+        ]
+    ]
+}
+
+return this
