3b2e9caf9f2e0fa4b4e7f0b8b84210087e286224
Sviatoslav Zastavskyi <zastavskyi.s@ajax.systems>
2024-11-18 18:39:32 +0200

    update generator

--- generate_scenario.py
+++ generate_scenario.py
@@ -2,14 +2,17 @@ import argparse
 import json
 from helpers import render_template, get_logger
 from templates import DEPLOY_SVC_TEMPLATE, RUN_BDD_TESTS_TEMPLATE, BASE_GROOVY_TEMPLATE
+from loguru import logger as LoguruLogger
+
+SCENARIOS_DIR = "scenarios"
 
 
 class RCGroovyScenarioBuilder:
-    def __init__(self, scenario_name, scenario_date):
+    def __init__(self, scenario_name, scenario_date, logger):
         self.scenario_name = scenario_name
         self.scenario_date = scenario_date
         self.stages = []
-        self.logger = get_logger()
+        self.logger = logger
 
     def get_builder_method(self, stage_name):
         return {
@@ -18,10 +21,6 @@ class RCGroovyScenarioBuilder:
         }.get(stage_name)
 
     def build_deploy_service_stage(self, serviceName, serviceVersion):
-        """
-            Deploy service with specified name and version
-            a911-svc {version": "1.111.0-7151.MASTER-SNAPSHOT"}
-        """
         stage_passed_variable = f"deploy_{serviceName.lower().replace('-', '_')}_passed"
 
         stage = render_template(
@@ -36,9 +35,7 @@ class RCGroovyScenarioBuilder:
         self.logger.info(f"Deploy {serviceName} {serviceVersion}")
 
     def build_run_bdd_tests_stage(self, marks):
-        """
-            Generate setups, respawn actors, unlock setups and run BDD tests with specified marks
-        """
+        """Generate setups, respawn actors, unlock setups and run BDD tests with specified marks"""
         stage_name = "Run BDD tests"
         if marks:
             marks = ", ".join(marks)
@@ -61,14 +58,17 @@ class RCGroovyScenarioBuilder:
             stages="\n".join((str(stage) for stage in self.stages))
         )
 
-        with open(f"scenarios/{self.scenario_date}.groovy", "w") as f:
+        groovy_file_path = f"{SCENARIOS_DIR}/{self.scenario_date}.groovy"
+        with open(groovy_file_path, "w") as f:
             f.write(groovy)
-        self.logger.success(f"File 'scenarios/{self.scenario_date}.groovy successfully generated")
 
-def generate_groovy_file(json_scenario: dict):
+        self.logger.success(f"File '{groovy_file_path}' successfully generated")
+
+def generate_groovy_file(json_scenario: dict, logger: LoguruLogger):
     builder = RCGroovyScenarioBuilder(
         scenario_name=json_scenario["scenarioName"],
         scenario_date=json_scenario["date"],
+        logger=logger,
     )
 
     for stage in json_scenario["stages"]:
@@ -78,7 +78,7 @@ def generate_groovy_file(json_scenario: dict):
     builder.save_groovy_file()
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--json_scenario_path",
@@ -87,8 +87,18 @@ if __name__ == "__main__":
         required=True
     )
     args = parser.parse_args()
+    logger = get_logger()
 
-    with open(args.json_scenario_path) as json_scenario_file:
-        scenario = json.load(fp=json_scenario_file)
+    try:
+        with open(args.json_scenario_path) as json_scenario_file:
+            scenario = json.load(fp=json_scenario_file)
+    except json.decoder.JSONDecodeError:
+        logger.error("Failed to parse JSON scenario")
+        exit(1)
 
-    generate_groovy_file(json_scenario=scenario)
+    generate_groovy_file(json_scenario=scenario, logger=logger)
+
+
+if __name__ == "__main__":
+    main()
+
