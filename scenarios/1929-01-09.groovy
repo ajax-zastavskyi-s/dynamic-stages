4a05eafb14fd8a8464ed3b60a48485509cd8db73
Kateryna Holub <holub.k@ajax.systems>
2025-01-16 14:43:31 +0200

    Update 1929-01-09.groovy

--- scenarios/1929-01-09.groovy
+++ scenarios/1929-01-09.groovy
@@ -19,70 +19,41 @@ def getStages() {
 
     return [
         [
-            name: "Run BDD tests",
+            name: "Deploy communication-svc 1.64.0*.RELEASE| company-svc 1.90.0*.RELEASE| cloud-signaling-svc 1.100.0.*.RELEASE| a911-svc 1.121.0*.RELEASE| user-svc 1.23.0*.RELEASE| image-svc 1.24.0*.RELEASE",
             steps: {
                 script {
-                    dynamicStagesResults = getDynamicStagesResults()
+                    parallel (
+                        
+                                "Deploy communication-svc 1.64.0*.RELEASE":{
+                                echo "DEPLOY communication-svc 1.64.0*.RELEASE"
+                                },
 
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.clearUsedObjects()
-                        rc_testing.respawnActors()
-                        dynamicStagesResults['setup_generation_passed'] = rc_testing.generateSetups()
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip setup generation due to failure: ${failedStage} == false"
-                    }
 
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.runBDDTests(
-                            marks='Empty',
-                            test_plan_name='RC [Undefined]'
-                        )
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key 
-                        echo "Skip running BDD tests due to failure: ${failedStage} == false"
-                        if (failedStage != "setup_generation_passed") {
-                            Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                        }
-                    }
+                                "Deploy company-svc 1.90.0*.RELEASE":{
+                                echo "DEPLOY company-svc 1.90.0*.RELEASE"
+                                },
 
-                    env.dynamicStagesResults = ""
-                }
-            }
-        ],
-        [
-            name: "Run BDD tests",
-            steps: {
-                script {
-                    dynamicStagesResults = getDynamicStagesResults()
 
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.clearUsedObjects()
-                        rc_testing.respawnActors()
-                        dynamicStagesResults['setup_generation_passed'] = rc_testing.generateSetups()
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip setup generation due to failure: ${failedStage} == false"
-                    }
+                                "Deploy cloud-signaling-svc 1.100.0.*.RELEASE":{
+                                echo "DEPLOY cloud-signaling-svc 1.100.0.*.RELEASE"
+                                },
 
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.runBDDTests(
-                            marks='Empty',
-                            test_plan_name='RC [Undefined]'
-                        )
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key 
-                        echo "Skip running BDD tests due to failure: ${failedStage} == false"
-                        if (failedStage != "setup_generation_passed") {
-                            Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                        }
-                    }
 
-                    env.dynamicStagesResults = ""
+                                "Deploy a911-svc 1.121.0*.RELEASE":{
+                                echo "DEPLOY a911-svc 1.121.0*.RELEASE"
+                                },
+
+
+                                "Deploy user-svc 1.23.0*.RELEASE":{
+                                echo "DEPLOY user-svc 1.23.0*.RELEASE"
+                                },
+
+
+                                "Deploy image-svc 1.24.0*.RELEASE":{
+                                echo "DEPLOY image-svc 1.24.0*.RELEASE"
+                                },
+
+                    )
                 }
             }
         ],
