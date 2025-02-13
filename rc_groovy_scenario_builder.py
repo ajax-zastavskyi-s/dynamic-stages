efb36d002e9f0f9fd446b5f4f51b78a7290e2518
Andrii Rishkiv <rishkiv.a@ajax.systems>
2025-02-13 15:06:51 +0200

    APQA-24135: Update templates and build process

--- rc_groovy_scenario_builder.py
+++ rc_groovy_scenario_builder.py
@@ -1,7 +1,9 @@
 from helpers import render_template
-from templates import DEPLOY_SVC_TEMPLATE, RUN_BDD_TESTS_TEMPLATE, BASE_GROOVY_TEMPLATE
+from templates import (BASE_GROOVY_TEMPLATE, DEPLOY_SVC_TEMPLATE,
+                       PARALLEL_DEPLOY_TEMPLATE, RUN_BDD_TESTS_TEMPLATE, SET_FF_TEMPLATE, PARALLEL_RESTORE_TEMPLATE)
 
 SCENARIOS_DIR = "scenarios"
+
 
 class RCGroovyScenarioBuilder:
     def __init__(self, scenario_name, scenario_date, logger):
@@ -10,29 +12,28 @@ class RCGroovyScenarioBuilder:
         self.stages = []
         self.logger = logger
         self.deployments = []
+        self.deployed_services = []
+        self.ff_toggles = []
 
     def get_builder_method(self, stage_name):
         return {
             "deployService": self.build_deploy_service_stage,
             "runTests": self.build_run_bdd_tests_stage,
+            "setFeatureFlag": self.build_set_ff_stage
         }.get(stage_name)
 
     def build_deploy_service_stage(self, serviceName, serviceVersion, deploymentDestination="null"):
         stage_passed_variable = f"deploy_{serviceName.lower().replace('-', '_')}_passed"
+        stage_name = f"{serviceName} {serviceVersion}"
 
-        stage = render_template(
-            template=DEPLOY_SVC_TEMPLATE,
-            stage_name=f"Deploy {serviceName} {serviceVersion}",
-            service_name=serviceName,
-            service_version=serviceVersion,
-            deployment_destination=deploymentDestination,
-            stage_passed_variable=stage_passed_variable,
-        )
-
-        self.stages.append(stage)
-        self.deployments.append(f"{serviceName} {serviceVersion}")
-
-        self.logger.info(f"Deploy {serviceName} {serviceVersion}")
+        self.deployments.append({
+            "stage_name": stage_name,
+            "service_name": serviceName,
+            "service_version": serviceVersion,
+            "deployment_destination": deploymentDestination,
+            "stage_passed_variable": stage_passed_variable,
+        })
+        self.deployed_services.append(stage_name)
 
     def build_run_bdd_tests_stage(self, marks):
         """Generate setups, respawn actors, unlock setups and run BDD tests with specified marks"""
@@ -43,11 +44,13 @@ class RCGroovyScenarioBuilder:
         else:
             marks = "Empty"
 
+        self._build_parallel_deployments_stage()
+        self._build_ff_stage()
+
+        test_plan_name = "RC [Undefined]"
+
         if self.deployments:
-            test_plan_name = " | ".join(self.deployments)
-            test_plan_name = f"RC [{test_plan_name}]"
-        else:
-            test_plan_name = "RC [Undefined]"
+            test_plan_name = f"RC [{' | '.join(self.deployed_services)}]"
 
         run_tests_stage = render_template(
             template=RUN_BDD_TESTS_TEMPLATE,
@@ -59,6 +62,19 @@ class RCGroovyScenarioBuilder:
         self.stages.append(run_tests_stage)
         self.logger.info(f"Run BDD tests with marks: {marks}")
         self.deployments = []
+
+    def build_set_ff_stage(self, serviceName, featureFlagName, featureFlagState, additionalData="null"):
+        stage_name = f"{featureFlagName} [{serviceName}]"
+        stage_passed_variable = f"restore_{featureFlagName.lower().replace('-', '_')}_passed"
+
+        self.ff_toggles.append({
+            "stage_name": stage_name,
+            "service_name": serviceName,
+            "feature_flag_name": featureFlagName,
+            "feature_flag_state": featureFlagState,
+            "additional_data": str(additionalData).replace("'", '"'),
+            "stage_passed_variable": stage_passed_variable,
+        })
 
     def save_groovy_file(self):
         groovy = render_template(
@@ -72,3 +88,32 @@ class RCGroovyScenarioBuilder:
             f.write(groovy)
 
         self.logger.success(f"File '{groovy_file_path}' successfully generated")
+
+    def _build_parallel_deployments_stage(self):
+        parallel_deployments_stage = render_template(
+            template=PARALLEL_DEPLOY_TEMPLATE,
+            stage_name=f"Deploy {self.rc_deployments}",
+            parallel_deployments="\n".join([render_template(template=DEPLOY_SVC_TEMPLATE, **deploy)
+                                            for deploy in self.deployments])
+        )
+        self.stages.append(parallel_deployments_stage)
+
+        self.logger.success(f"Deploy {self.rc_deployments}")
+
+    def _build_ff_stage(self):
+        build_ff_stage = render_template(
+            template=PARALLEL_RESTORE_TEMPLATE,
+            stage_name=f"Restore toggles {self.rc_restoring}",
+            parallel_deployments="\n".join([render_template(template=SET_FF_TEMPLATE, **toggle)
+                                            for toggle in self.ff_toggles])
+        )
+        self.stages.append(build_ff_stage)
+        self.logger.success(f"Restore {self.rc_restoring}")
+
+    @property
+    def rc_deployments(self):
+        return " | ".join([deploy['stage_name'] for deploy in self.deployments])
+
+    @property
+    def rc_restoring(self):
+        return " | ".join([toggle['stage_name'] for toggle in self.ff_toggles])
--- templates.py
+++ templates.py
@@ -29,34 +29,57 @@ return this
 
 """)
 
-DEPLOY_SVC_TEMPLATE = Template("""
+PARALLEL_DEPLOY_TEMPLATE = Template("""
         [
             name: "$stage_name",
             steps: {
                 script {
-                    dynamicStagesResults = getDynamicStagesResults()
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        def serviceVersionFromPattern = rc_testing.getLatestServiceVersionByPattern(
-                            serviceName="$service_name",
-                            serviceVersionPattern="$service_version"
-                        )
-
-                        dynamicStagesResults['$stage_passed_variable'] = rc_testing.deployService(
-                            serviceName="$service_name",
-                            serviceVersion=serviceVersionFromPattern,
-                            deploymentDestination="$deployment_destination",
-                        )
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip deploy due to failure: ${failedStage} == false"
-                        Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                    }
-
-                    env.dynamicStagesResults = groovy.json.JsonOutput.toJson(dynamicStagesResults)
+                    parallel (
+$parallel_deployments
+                    )
                 }
             }
         ],
+""")
+
+PARALLEL_RESTORE_TEMPLATE = Template("""
+        [
+            name: "$stage_name",
+            steps: {
+                script {
+                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
+                        parallel (
+$parallel_deployments
+                        )
+                    } else {
+                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
+                        echo "Skip restore toggles due to failure: ${failedStage} == false"
+                    }
+                }
+            }
+        ],
+""")
+
+DEPLOY_SVC_TEMPLATE = Template("""
+                        "$stage_name": {
+                            dynamicStagesResults = getDynamicStagesResults()
+
+                            def serviceVersionFromPattern = rc_testing.getLatestServiceVersionByPattern(
+                                serviceName="$service_name",
+                                serviceVersionPattern="$service_version"
+                            )
+
+                            dynamicStagesResults['$stage_passed_variable'] = rc_testing.deployService(
+                                serviceName="$service_name",
+                                serviceVersion=serviceVersionFromPattern,
+                                deploymentDestination="$deployment_destination",
+                            )
+                            if (dynamicStagesResults['$stage_passed_variable'] == false) {
+                                saveFailedDeploy("$service_name", "$service_version")
+                            }
+
+                            env.dynamicStagesResults = groovy.json.JsonOutput.toJson(dynamicStagesResults)
+                        },
 """)
 
 RUN_BDD_TESTS_TEMPLATE = Template("""
@@ -94,4 +117,22 @@ RUN_BDD_TESTS_TEMPLATE = Template("""
                 }
             }
         ],
+""")
+
+SET_FF_TEMPLATE = Template("""
+                        "$stage_name": {
+                            dynamicStagesResults = getDynamicStagesResults()
+
+                            dynamicStagesResults['$stage_passed_variable'] = rc_testing.restoreToggle(
+                                serviceName='$service_name',
+                                featureFlagName='$feature_flag_name',
+                                featureFlagState='$feature_flag_state',
+                                additionalData='$additional_data'
+                            )
+                            if (dynamicStagesResults['$stage_passed_variable'] == false) {
+                                saveFailedDeploy("$feature_flag_name", "$service_name")
+                            }
+
+                            env.dynamicStagesResults = groovy.json.JsonOutput.toJson(dynamicStagesResults)
+                        },
 """)
